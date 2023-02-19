9d7368d64e6d0fb62ffe67f39936caf0b8e37800
JamesBond000077 <125532443+JamesBond000077@users.noreply.github.com>
2023-02-19 21:30:50 +0530

    Update streamlit_app.py
    added function insert_row_snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,4 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
