c87d79f586bb33a7d312f4d88148572b869868c2
JamesBond000077 <125532443+JamesBond000077@users.noreply.github.com>
2023-02-19 22:31:36 +0530

    Update streamlit_app.py
    edited try block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 from urllib.error import URLError
+import pandas
+import requests
+import snowflake.connector
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -10,7 +13,7 @@ streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+# import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -21,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-def get_fruityvice_date(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -29,17 +32,19 @@ def get_fruityvice_date(this_fruit_choice):
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function = get_fruityvice_date(fruit_choice)
-    streamlit.dataframe(back_from_function)
+    fruit_choice = streamlit.text_input("What fruit would you like information about?")
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
+
   
 except URLError as e:
   streamlit.error()
   
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
@@ -48,7 +53,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
-import snowflake.connector
+# import snowflake.connector
 
 streamlit.header("The fruit load list contains:")
 
